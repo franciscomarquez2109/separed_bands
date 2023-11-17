34c4d7d154d510b9a5a272807dd0e044c393fa6e
Francisco Marquez <franciscomarquez210993@gmail.com>
2023-11-17 13:21:53 -0400

    fix: Error final al copiar los archivos

--- separate_bands.py
+++ separate_bands.py
@@ -5,24 +5,24 @@ import shutil
 # arreglo con las expresiones regulares
 regularExpressions = [r'550nm',r'660nm',r'790nm',r'735nm']
 # Directorio de origen donde se buscarán los archivos JPG
-sourceDirectory = "."
+sourceDirectory = "/Users/franciscomarquez/Pictures/venturino_1"
 # Directorio de destino donde se crearán las carpetas y se moverán los archivos
-destinationDirectory = "./bandas"
+destinationDirectory = "/Users/franciscomarquez/Pictures/venturino_1/bandas"
 
 # Crear directorio de destino si no existe
 if not os.path.exists(destinationDirectory):
     os.makedirs(destinationDirectory)
 
 # Obtener la lista de archivos JPG en el directorio de origen
-tifFiles = [file for file in os.listdir(sourceDirectory) if file.lower().endswith('.tif')]
+tifFiles = [tifFile for tifFile in os.listdir(sourceDirectory) if tifFile.lower().endswith('.tif')]
 
 # Iterar sobre los archivos tif y moverlos a carpetas correspondientes
-for file in tifFiles:
+for tifFile in tifFiles:
     #Iterar sobre las expresiones regulares para buscar cada una de ellas en la lista de bandas
     for expression in regularExpressions:
         rowExpression = re.compile(expression)
          # Buscar la coincidencia con la expresión regular en el nombre del archivo
-        coincidences = rowExpression.findall(file)
+        coincidences = rowExpression.findall(tifFile)
 
         if len(coincidences) > 0:
 
@@ -30,37 +30,37 @@ for file in tifFiles:
             folderName = coincidences[0]
 
             # Crear directorio para el nombre si no existe
-            diretoryName = os.path.join(destinationDirectory, folderName)
-            if not os.path.exists(diretoryName):
-                os.makedirs(diretoryName)
+            directoryName = os.path.join(sourceDirectory, folderName)
+            if not os.path.exists(directoryName):
+                os.makedirs(directoryName)
 
             # Ruta completa del archivo de origen y destino
-            originRoute = os.path.join(sourceDirectory, file)
-            destinationRoute = os.path.join(folderName, file)
+            originRoute = os.path.join(sourceDirectory, tifFile)
+            destinationRoute = os.path.join(directoryName, tifFile)
 
             # copiar el archivo a la carpeta correspondiente
             shutil.copy(originRoute, destinationRoute)
-            print(f"Copiando '{file}' a '{diretoryName}'")
+            print(f"Copiando '{tifFile}' a '{directoryName}'")
             break
 
 #obtenemos la lista de imagenes jpg para agregarlas a una carpeta en el mismo destino
-files_jpg = [file for file in os.listdir(sourceDirectory) if file.lower().endswith('.jpg')]
+jpgFiles = [jpgFile for jpgFile in os.listdir(sourceDirectory) if jpgFile.lower().endswith('.jpg')]
 
 # nombre unico para la carpeta de imagenes
 folderName = 'RGB'
 
  # Crear directorio para el nombre si no existe
-diretoryName = os.path.join(destinationDirectory, folderName)
-if not os.path.exists(diretoryName):
-    os.makedirs(diretoryName)
+directoryName = os.path.join(destinationDirectory, folderName)
+if not os.path.exists(directoryName):
+    os.makedirs(directoryName)
 
 # Iterar sobre los archivos JPG y moverlos a carpetas correspondientes
-for file in files_jpg:
+for jpgFile in jpgFiles:
    
     # Ruta completa del archivo de origen y destino
-    originRoute = os.path.join(sourceDirectory, file)
-    destinationRoute = os.path.join(diretoryName, file)
+    originRoute = os.path.join(sourceDirectory, jpgFile)
+    destinationRoute = os.path.join(directoryName, jpgFile)
 
     # Mover el archivo a la carpeta correspondiente
-    shutil.copy(sourceDirectory, destinationDirectory)
-    print(f"Copiando '{file}' a '{diretoryName}'")
+    shutil.copy(originRoute, destinationRoute)
+    print(f"Copiando '{jpgFile}' a '{directoryName}'")
